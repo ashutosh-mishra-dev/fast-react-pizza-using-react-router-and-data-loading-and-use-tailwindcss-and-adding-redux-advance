8d13a7dbf43fe479edaf6955b96276334c5031e4
ashutosh mishra <mishraashutosh.dev@gmail.com>
2025-10-07 13:20:12 +0530

    update in section 22 : react router with data loading : A new way to implementing routes using createBrowserRouter in data mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-function App() {
-  const x = 23;
-  console.log(x);
+// router version 6 : createBrowserRouter, RouterProvider
+// router version 7 : createBrowserRouter, RouterProvider
 
-  return <div>hello vite</div>;
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  { path: "/cart", element: <Cart /> },
+  { path: "/order/new", element: <CreateOrder /> },
+  { path: "/order/:orderId", element: <Order /> },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
