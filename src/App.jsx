4f825d2acc807a176966989abad859e2a4ce509d
ashutosh mishra <mishraashutosh.dev@gmail.com>
2025-10-08 10:05:06 +0530

    update in section 22 : react router with data loading : here handling error with ErrorBoundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
-// router version 6.4 : createBrowserRouter, RouterProvider
-// router version 7.9 : createBrowserRouter, RouterProvider
+//------------------------- react router v 6.4  --------------------------------------------------------------------
+// import { createBrowserRouter, RouterProvider } from "react-router";
+// import Home from "./ui/Home";
+// import Menu, { Loader as menuLoader } from "./features/menu/Menu";
+// import Cart from "./features/cart/Cart";
+// import CreateOrder from "./features/order/CreateOrder";
+// import Order from "./features/order/Order";
+// import AppLayout from "./ui/AppLayout";
+
+// const router = createBrowserRouter([
+//   {
+//     element: <AppLayout />,
+//     children: [
+//       { path: "/", element: <Home /> },
+//       {
+//         path: "/menu",
+//         element: <Menu />,
+//         loader: menuLoader,
+//       },
+//       { path: "/cart", element: <Cart /> },
+//       { path: "/order/new", element: <CreateOrder /> },
+//       { path: "/order/:orderId", element: <Order /> },
+//     ],
+//   },
+// ]);
+
+// function App() {
+//   return <RouterProvider router={router} />;
+// }
+
+// export default App;
+
+//------------------------- react router v 7.9 --------------------------------------------------------------------
 
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Home from "./ui/Home";
@@ -8,20 +39,22 @@ import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
-    element: <AppLayout />,
+    Component: AppLayout, // layout component
+    ErrorBoundary: Error,
     children: [
-      { path: "/", element: <Home /> },
+      { index: true, Component: Home }, // / -> Home
       {
-        path: "/menu",
-        element: <Menu />,
-        loader: menuLoader,
+        path: "menu",
+        Component: Menu,
+        loader: menuLoader, // loader for menu data
       },
-      { path: "/cart", element: <Cart /> },
-      { path: "/order/new", element: <CreateOrder /> },
-      { path: "/order/:orderId", element: <Order /> },
+      { path: "cart", Component: Cart },
+      { path: "order/new", Component: CreateOrder },
+      { path: "order/:orderId", Component: Order },
     ],
   },
 ]);
--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,12 +1,18 @@
 import { useNavigate } from "react-router";
-
+import { useRouteError } from "react-router";
 function NotFound() {
   const navigate = useNavigate();
+  const error = useRouteError();
+  console.log(error);
 
   return (
     <div>
       <h1>Something went wrong 😢</h1>
       <p>%MESSAGE%</p>
+      <p>
+        <span>{error.status} </span>
+        {error.statusText || error.message}
+      </p>
       <button onClick={() => navigate(-1)}>&larr; Go back</button>
     </div>
   );
