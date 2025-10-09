afd2b5edd7bf4fc92bbe04cd00562bd53dd6e8da
ashutosh mishra <mishraashutosh.dev@gmail.com>
2025-10-09 16:18:48 +0530

    update in section 23 : Tailwind css : responsive design

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Ashutosh</div>;
+  return <div className="hidden text-sm font-semibold sm:block">Ashutosh</div>;
 }
 
 export default Username;
