39370fd83898a722c2461799da375844357d7c18
ashutosh mishra <mishraashutosh.dev@gmail.com>
2025-10-13 12:21:30 +0530

    update in section 23 : Tailwind css : styling the order form

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router";
 
 function Button({ children, disabled, to, type }) {
   const base =
-    "inline-block rounded-full bg-yellow-400  font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed cursor-pointer ";
+    "inline-block text-sm rounded-full bg-yellow-400  font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed cursor-pointer ";
 
   const styles = {
     primary: base + "py-3 px-4 md:px-6 md:py-4",
     small: base + "py-2 px-4 md:px-5 md:py-2.5 text-xs",
     secondary:
-      "inline-block rounded-full border-2 border-stone-300 cursor-pointer font-semibold tracking-wide uppercase text-stone-400  transition-colors duration-300 hover:bg-stone-300  hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
+      "inline-block text-sm rounded-full border-2 border-stone-300 cursor-pointer font-semibold tracking-wide uppercase text-stone-400  transition-colors duration-300 hover:bg-stone-300  hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
   };
 
   if (to)
