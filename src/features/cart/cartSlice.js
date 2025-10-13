90a2ede72799b9c20f916ce016d6441609f28055
ashutosh mishra <mishraashutosh.dev@gmail.com>
2025-10-13 18:19:54 +0530

    update in section 24 : Adding Redux and Advanced React Router : modeling the cart state here we are working same as user state mean creating cartSlice update stor.js

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,55 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [
+    {
+      pizzaId: 1,
+      name: "abc",
+      unitPrice: 30,
+      quantity: 12,
+      totalPrice: 240,
+    },
+  ],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      //payload = newItem
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      //payload = pizzaId
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      //payload = pizzaId
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+
+      item.quantity++; // yha pr ham redux-toollit ka use kar rhe h es liye ham mutate data pass kar pa rhe h baki react me ham es prkar se data pass nhi karte
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    decreaseeItemQuantity(state, action) {
+      //payload = pizzaId
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+
+      item.quantity--; // react me ham es prkar se data pass nhi karte
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseeItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
