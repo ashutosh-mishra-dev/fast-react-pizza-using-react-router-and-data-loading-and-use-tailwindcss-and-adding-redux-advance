31e0fdc4ddfbc7faf4fa31d22f25e70e3cccb3b1
ashutosh mishra <mishraashutosh.dev@gmail.com>
2025-10-14 15:00:35 +0530

    update in section 24 : Adding Redux and Advanced React Router : here Adding menu items to the cart reducer

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,7 +1,21 @@
 import { formatCurrency } from "../../utils/helpers";
 import Button from "../../ui/Button";
+import { useDispatch } from "react-redux";
+import { addItem } from "../cart/cartSlice";
+
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+  function handleClick() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      unitPrice,
+      quantity: 1,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+  }
 
   return (
     <li className="flex gap-4 py-2">
@@ -23,9 +37,11 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          <Button type="small" disabled={`${soldOut ? "true" : ""}`}>
-            add to cart
-          </Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleClick}>
+              add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
